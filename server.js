0cb54d5d6c3ac7f4bf5b41e8a29f2c5bc6443484
Carlos Parraga <carlosparraga90@hotmail.com>
2023-04-08 16:29:04 -0400

    Change complete to true in completed tasks

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const pendingTasks = [
 ];
 
 const completedTasks = [
-  new Task(1, 'Run the code', dat('2023-04-07 10:30'), false),
-  new Task(3, 'Make the appointment', dat('2023-04-03 16:59'), false),
-  new Task(4, 'Play the game', dat('2023-03-25 17:25'), false),
+  new Task(1, 'Run the code', dat('2023-04-07 10:30'), true),
+  new Task(3, 'Make the appointment', dat('2023-04-03 16:59'), true),
+  new Task(4, 'Play the game', dat('2023-03-25 17:25'), true),
 ];
 
 const app = express();
