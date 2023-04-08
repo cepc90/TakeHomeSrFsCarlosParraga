4bfbfc27809da81d61416bf17f0f621fc7cbbeec
Carlos Parraga <carlosparraga90@hotmail.com>
2023-04-08 10:55:29 -0400

    Remove file caching as it was not requested or necessary. gitignore ide files. Rename file to index.html

--- server.js
+++ server.js
@@ -2,23 +2,15 @@ const http = require('http');
 const express = require('express');
 const fs = require('fs');
 
-let indexHtml;
-
-fs.readFile('hello.html', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-  } else {
-    indexHtml = data;
-  }
-});
-
 const app = express();
 app.get('/', (req, res) => {
-  if (indexHtml) {
-    res.send(indexHtml);
-  } else {
-    res.status(500).send('Error: Unable to render HTML file');
-  }
+  fs.readFile('index.html', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error: Unable to render HTML file');
+    } else {
+      res.send(data);
+    }
+  });
 });
 
 app.listen(8080, () => {
