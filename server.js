dcee677a31ebcc8fa899f5be3b9b9820274426c9
Carlos Parraga <carlosparraga90@hotmail.com>
2023-04-08 11:25:33 -0400

    Add boostrap as a dependency and serve then index.html file from the public directory

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const http = require('http');
 const express = require('express');
-const fs = require('fs');
 
 const app = express();
-app.get('/', (req, res) => {
-  fs.readFile('index.html', 'utf8', (err, data) => {
-    if (err) {
-      res.status(500).send('Error: Unable to render HTML file');
-    } else {
-      res.send(data);
-    }
-  });
-});
-
+app.use(express.static('public'));
 app.listen(8080, () => {
   console.log('Server listening on port 8080');
 });
