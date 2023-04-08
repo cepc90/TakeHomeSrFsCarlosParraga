202994d4604d5a5db4e944f3d0f67fffd778df61
Carlos Parraga <carlosparraga90@hotmail.com>
2023-04-08 16:28:03 -0400

    Respond hard coded tasks in the endpoinds /api/completedTasks and /api/pendingTasks

---
+++ Task.js
@@ -0,0 +1,10 @@
+class Task {
+    constructor(id, description, completedDate, completed) {
+        this.id = id;
+        this.description = description;
+        this.completedDate = completedDate;
+        this.completed = completed;
+    }
+}
+
+module.exports = Task;
--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 const express = require('express');
+const Task = require("./Task");
+const moment = require('moment-timezone');
+
+function dat(string) {
+  return moment.tz(string, 'Canada/Eastern').toDate();
+}
+
+const pendingTasks = [
+  new Task(1, 'Run the code', null, false),
+  new Task(3, 'Make the appointment', null, false),
+  new Task(4, 'Play the game', null, false),
+];
+
+const completedTasks = [
+  new Task(1, 'Run the code', dat('2023-04-07 10:30'), false),
+  new Task(3, 'Make the appointment', dat('2023-04-03 16:59'), false),
+  new Task(4, 'Play the game', dat('2023-03-25 17:25'), false),
+];
 
 const app = express();
+
+app.get('/api/completedTasks', (req, res) => {
+  res.json(completedTasks);
+});
+
+app.get('/api/pendingTasks', (req, res) => {
+  res.json(pendingTasks);
+});
+
 app.use(express.static('public'));
 app.listen(8080, () => {
   console.log('Server listening on port 8080');
