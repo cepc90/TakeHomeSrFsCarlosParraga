716af2ee8c04d286928fc24706801cbd3592774b
Carlos Parraga <carlosparraga90@hotmail.com>
2023-04-09 19:33:51 -0400

    Use the database to store the list of tasks

--- server.js
+++ server.js
@@ -1,24 +1,85 @@
 const express = require('express');
 const cors = require('cors');
 const Task = require("./Task");
-const moment = require('moment-timezone');
+const {existsSync} = require("fs");
+const sqlite3 = require('sqlite3');
 
-function dat(string) {
-  return moment.tz(string, 'Canada/Eastern').toDate();
+const pendingTasks = [];
+const completedTasks = [];
+
+const DB_PATH = './task_database.sqlite';
+let db;
+if (!existsSync(DB_PATH)) { // It will create it if it doesn't exist, so I have to create the connection after checking if it exists
+  db = new sqlite3.Database(DB_PATH);
+  db.run('CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT, completedDate DATETIME, completed BOOLEAN);', function(err) {
+    if (err) {
+      console.log(err.message);
+    } else {
+      console.log('Table created successfully.');
+
+      saveTaskToDb('Run the code', function () {
+        saveTaskToDb('Make the appointment', function () {
+          saveTaskToDb('Play the game', function () {
+            saveCompletedTaskToDb('Play with child', '2023-04-07 10:30', function () {
+              saveCompletedTaskToDb('Go Easter Egg hunting', '2023-04-03 16:59', function () {
+                saveCompletedTaskToDb('Have breakfast', '2023-03-25 17:25', function () {
+                  loadTasksFromTheDatabase();
+                });
+              });
+            });
+          });
+        });
+      });
+
+    }
+  });
+} else {
+  db = new sqlite3.Database(DB_PATH);
+  loadTasksFromTheDatabase();
 }
 
-const pendingTasks = [
-  new Task(1, 'Run the code', null, false),
-  new Task(3, 'Make the appointment', null, false),
-  new Task(4, 'Play the game', null, false),
-];
+function loadTasksFromTheDatabase() {
+  db.all('SELECT * FROM tasks', [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
 
-const completedTasks = [
-  new Task(2, 'Play with child', dat('2023-04-07 10:30'), true),
-  new Task(5, 'Go Easter Egg hunting', dat('2023-04-03 16:59'), true),
-  new Task(6, 'Have breakfast', dat('2023-03-25 17:25'), true),
-];
+    const tasks = rows.map(row => ({
+      id: row.id,
+      description: row.description,
+      completedDate: row.completedDate,
+      completed: Boolean(row.completed),
+    }));
 
+    for (const task of tasks) {
+      if (task.completed) {
+        completedTasks.push(task);
+      } else {
+        pendingTasks.push(task);
+      }
+    }
+  });
+}
+
+function saveTaskToDb(description, callback) {
+  db.run('INSERT INTO tasks (description, completedDate, completed) VALUES (?, NULL, 0)', [description], function(err) {
+    if (err) {
+      console.log('Error inserting task: ', err);
+    } else {
+      callback(db.lastID);
+    }
+  });
+}
+
+function saveCompletedTaskToDb(description, completedDate, callback) {
+  db.run('INSERT INTO tasks (description, completedDate, completed) VALUES (?, ?, 1)', [description, completedDate], function(err) {
+    if (err) {
+      console.log('Error inserting task: ', err);
+    } else {
+      callback(db.lastID);
+    }
+  });
+}
 
 const app = express();
 app.use(cors()); // Was added for
@@ -29,6 +90,15 @@ app.get('/api/completedTasks', (req, res) => {
 
 app.get('/api/pendingTasks', (req, res) => {
   res.json(pendingTasks);
+});
+
+app.post('/api/task', (req, res) => {
+  const { description } = req.body;
+  saveTaskToDb(description, function (id) {
+    const task = new Task(id, description, null, false);
+    pendingTasks.push(task);
+    res.status(201).send(`Task "${description}" created successfully`);
+  });
 });
 
 app.use(express.static('public'));
